cffb97c2afbf153a0236704fbff91b19f9afc711
wimaks <squ1rtle.mg@gmail.com>
2022-12-05 01:26:50 +0600

    Сверстал еще несколько блоков, заканчивая delivery

--- js/app.js
+++ js/app.js
@@ -12,4 +12,30 @@ $(document).ready(function () {
         }]
     });
 
+    //* BURGER ACTIVE
+
+    $('.nav-toggle').on('click', function (event) {
+        event.preventDefault();
+
+        $('.burger').toggleClass('active'); // появление меню при нажатии на бургер
+        $(this).toggleClass('active'); // изменение меню бургер при открытии меню
+    });
+
+    // * ACCORDEON ACTIVE
+
+    $('.faq-accordion__item').on('click', function (event) {
+        event.preventDefault();
+
+        $(this).toggleClass('active');
+    })
+
+    //* FAQ
+
+    $('.faq__overflow').on('click', function (event) {
+        event.preventDefault()
+
+        $('.faq-accordion').toggleClass('active')
+    })
+
 });
+
